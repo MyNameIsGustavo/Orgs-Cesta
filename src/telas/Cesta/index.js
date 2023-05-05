456f35cfe2cd382c384afdf6cdd3c14ceb6cc86e
MyNameIsGustavo <gustavo.professional.contact@gmail.com>
2023-05-04 23:23:44 -0300

    update da primeira versao do orgs-cesta

---
+++ src/telas/Cesta/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {StyleSheet, View} from 'react-native';
+import Topo from "./Componentes/Topo";
+import Detalhes from "./Componentes/Detalhes";
+
+export default function Cesta({topo, detalhes}){
+    return <> 
+        <Topo {...topo}/>
+        <View style={estilos.cesta}>
+            <Detalhes {...detalhes}/>
+        </View>
+    </>
+}
+
+const estilos = StyleSheet.create({
+    cesta:{
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    }
+});
