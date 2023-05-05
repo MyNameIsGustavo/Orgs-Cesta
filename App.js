3430d3240228c2813b48a88fe4c79ee97a99a9af
MyNameIsGustavo <gustavo.rochamaia@gmail.com>
2023-05-05 12:45:22 -0300

    finalizando o primeiro curso de react-native

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, View } from 'react-native';
-import AppLoading from 'expo-app-loading';
+
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from  '@expo-google-fonts/montserrat';
 import Cesta from './src/telas/Cesta';
 
 import mock from './src/mocks/mock_Cesta';
-
 
 export default function App() {
   const [fonteCarregada] = useFonts({
@@ -14,7 +13,7 @@ export default function App() {
   });
 
   if(!fonteCarregada){
-    return <AppLoading/>
+    return <View/>
   }
 
   return (
--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,18 +1,37 @@
 import React from "react";
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, FlatList} from 'react-native';
+
 import Topo from "./Componentes/Topo";
 import Detalhes from "./Componentes/Detalhes";
+import Item from "./Componentes/Item";
 
-export default function Cesta({topo, detalhes}){
-    return <> 
-        <Topo {...topo}/>
-        <View style={estilos.cesta}>
-            <Detalhes {...detalhes}/>
-        </View>
+export default function Cesta({topo, detalhes, itens}){
+    return <>
+        <FlatList 
+            data={itens.lista}
+            renderItem={Item}
+            keyExtractor={({nome}) => nome}
+            ListHeaderComponent={() =>{
+            return <>
+                <Topo {...topo}/>
+                <View style={estilos.cesta}>
+                    <Detalhes {...detalhes} />
+                </View>
+            </>
+        }}
+        />
     </>
 }
 
 const estilos = StyleSheet.create({
+    titulo:{
+        color: "#464646",
+        fontWeight: "bold",
+        marginTop: 32,
+        marginBottom: 8,
+        fontSize: 20,
+        lineHeight: 32,
+    },
     cesta:{
         paddingVertical: 8,
         paddingHorizontal: 16,
